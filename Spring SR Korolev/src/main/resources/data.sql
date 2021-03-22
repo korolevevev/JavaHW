27ad487abdcec86c35aa9da98a0f4c97e11e0f37
korolevevev <64105968+korolevevev@users.noreply.github.com>
2021-03-22 11:04:25 +0300

    Add files via upload

---
+++ Spring SR Korolev/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+    INSERT INTO news(name, userName, created, updated, body, catTitle)
+VALUES ('RapLect', 'Lil Pump', '20/01/2020', '22/02/2020', 'This is text!', 'Music'),
+('Putin', 'Jason Jackson', '29/07/2020', '11/01/2021', 'he is a president of Russia!', 'Politics'),
+('Spring', 'Mark Zukerberg', '21/03/2021', '22/03/2021', 'not worth', 'IT');
