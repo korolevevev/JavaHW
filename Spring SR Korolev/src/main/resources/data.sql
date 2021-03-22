4d9f303417ac32163be3e3b94a1e120ade1c4ea7
korolevevev <64105968+korolevevev@users.noreply.github.com>
2021-03-22 10:59:52 +0300

    Delete data.sql

--- Spring SR Korolev/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-    INSERT INTO news(name, userName, created, updated, text, catTitle)
-VALUES ('news1', 'Lil Pump', '20/09/2020', '21/03/2021', 'This is texttttxtxtxtxtxx', 'IT'),
-('news1', 'JAck Harlow', '13/01/2021', '14/01/2021', 'Thisrifj hola verde!', 'Design');
